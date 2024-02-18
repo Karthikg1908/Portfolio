e4990a00385f2cb7685287be73d08b490b9fd54c
Karthik <86306862+Karthikg1908@users.noreply.github.com>
2024-02-18 17:01:34 +0530

    Update

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Front End Devloper", "Back End Devloper", "Web Design"],
+        strings: ["Front End Devloper", "Back End Devloper", "Web Design","Front End Devloper"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
